4863a051068658476f353d04803687260b924528
shiqifriend <104892011+shiqifriend@users.noreply.github.com>
2023-03-05 17:04:52 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,14 @@ import VueSetupExtend from "vite-plugin-vue-setup-extend";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://xxxxxx",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [vue(), VueSetupExtend()],
 });
